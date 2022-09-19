f324cb11f6015199428e1ea2e9f7082e8871b589
davidv <davidvazquezgit@gmail.com>
2022-09-19 16:13:41 -0700

    mount the route, tell app to listen to port 3000

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-import { Express } from "express";
+import express from "express";
+
+//create express app
+const app = express()
+// configuer the app(app.set)
+
+
+//mount routes
+app.get('/', function (req,res) {
+  res.send('<h1> hello, Im your teacher</h1>')
+})
+
+//tell the app to listen on port 3000
+app.listen(3000, function(){
+  console.log("this is listening")
+})
+
