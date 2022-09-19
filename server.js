1334e39f9da9affce8587feae36d61403bba05e2
davidv <davidvazquezgit@gmail.com>
2022-09-19 16:55:44 -0700

    refactor the root route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine','ejs')
 
 //mount routes
 app.get('/', function (req,res) {
-  res.send('<h1> hello, Im your teacher</h1>')
+  res.redirect('/home')
 })
 
 app.get('/home', function(req,res) {
