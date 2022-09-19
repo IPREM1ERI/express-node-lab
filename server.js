b52416f75aedf6c5069899a0ca657eccd16f5966
davidv <davidvazquezgit@gmail.com>
2022-09-19 16:37:18 -0700

    make students dir in views dir, touch an idenx.ejs file in students dir

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/home', function(req,res) {
   res.render('home')
 })
 
+app.get('/students', function(req,res) {
+  res.render('students/index')
+})
+
 //tell the app to listen on port 3000
 app.listen(3000, function(){
   console.log('this is listening')
